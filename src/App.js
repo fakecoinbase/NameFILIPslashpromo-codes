bc3cd8e884e783d987bf303828a67a03c097ea26
Filip Spiridonov <fspiridonov@uber.com>
2020-04-08 18:05:23 -0700

    Build a simple UI

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+import { styled } from "baseui";
+import { H1, H2, H3, H4, H5, H6 } from "baseui/typography";
+import { StyledLink } from "baseui/link";
+import { Card, StyledBody, StyledAction } from "baseui/card";
+
+// import logo from "./logo.svg";
+// import "./App.css";
+import { promoCodes } from "./promo-codes";
+
+/* <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+    </div> */
+
+const Container = styled("div", {
+  margin: "20px",
+});
+
+const byCategory = promoCodes.reduce((result, item) => {
+  item.categories.forEach((category) => {
+    result[category] = (result[category] || []).concat(item);
+  });
+  return result;
+}, {});
+
+const cardOverrides = {
+  Root: {
+    style: {
+      margin: "10px",
+    },
+  },
+};
+
+function Item(props) {
+  const { item } = props;
+  return (
+    <Card title={item.name} overrides={cardOverrides}>
+      <StyledBody>
+        {item.description && <div>{item.description}</div>}
+        {item.code && <div>Code: {item.code}</div>}
+        <StyledLink href={item.link}>{item.link}</StyledLink>
+      </StyledBody>
+    </Card>
+  );
+}
+
+function Items(props) {
+  const { title, items } = props;
+  return (
+    <div>
+      <H2>{title}</H2>
+      {items.map((item) => (
+        <Item item={item} />
+      ))}
+    </div>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <H1>Promo Codes</H1>
+      {Object.keys(byCategory).map((category) => (
+        <Items title={category} items={byCategory[category]} />
+      ))}
+    </Container>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,14 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+
+import {BaseProvider, LightTheme} from 'baseui';
+import { Provider as StyletronProvider } from "styletron-react";
+import { Client as Styletron } from "styletron-engine-atomic";
+
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+
+const engine = new Styletron();
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <StyletronProvider value={engine}>
+      <BaseProvider theme={LightTheme}>
+        <App />
+      </BaseProvider>
+    </StyletronProvider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
