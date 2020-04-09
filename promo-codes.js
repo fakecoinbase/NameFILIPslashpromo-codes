0973a29f7c9469311c01c49952bedd5ad1c11aa5
Filip Spiridonov <fspiridonov@uber.com>
2020-04-08 17:05:24 -0700

    Reformat promo codes to a JS file

---
+++ promo-codes.js
@@ -0,0 +1,121 @@
+const products = [
+  {
+    name: "Blackbird",
+    description:
+      "Blackbird gives you the private flight experience at a budget airline price.",
+    code: "AI0AQ",
+    link: null,
+    categories: ["transportation"],
+  },
+  {
+    name: "Uber",
+    description:
+      "Uber Technologies Inc. is a global taxi technology company, operating in 633 cities worldwide.",
+    code: "uber15us",
+    link: null,
+    categories: ["transportation"],
+  },
+  {
+    name: "HotelTonight",
+    description:
+      "Amazing last-minute deals at top-rated hotels. Book Tonight, Tomorrow or 7 Days Out.",
+    code: "FSPIRIDONOV",
+    link: null,
+    categories: ["housing"],
+  },
+  {
+    name: "Airbnb",
+    description:
+      "Airbnb is the best way to rent unique, local accommodations on any travel budget.",
+    code: "fspiridonov",
+    link: "https://www.airbnb.com/c/fspiridonov",
+    categories: ["housing"],
+  },
+  {
+    name: "Robinhood",
+    description:
+      "Robinhood lets you learn to invest in the stock market for free.",
+    code: "filips2",
+    link: "http://share.robinhood.com/filips2",
+    categories: ["finance"],
+  },
+  {
+    name: "Booking",
+    description: "",
+    code: "",
+    link: "https://www.booking.com/s/27_8/c065ec84",
+    categories: ["housing"],
+  },
+  {
+    name: "Blind",
+    description: "Blind is an anonymous chat app for tech employees.",
+    code: "nXNnHm7f",
+    link: "https://us.teamblind.com/invite/nXNnHm7f",
+    categories: ["social"],
+  },
+  {
+    name: "Getaround",
+    description:
+      "Getaround is an online car sharing or peer-to-peer carsharing service",
+    code: "",
+    link: "https://www.getaround.com/invite/10210287264472979",
+    categories: ["transportation"],
+  },
+  {
+    name: "Turo",
+    description:
+      "Turo is a company that operates a peer-to-peer carsharing marketplace.",
+    code: "filips19",
+    link: "https://turo.com/c/filips19",
+    categories: ["transportation"],
+  },
+  {
+    name: "Scoot",
+    description: "SF Scooter Rental",
+    code: "filips3",
+    link: "https://turo.com/c/filips19",
+    categories: ["transportation"],
+  },
+  {
+    name: "Instacart",
+    description: "Instacart is a sometimes-same-day grocery delivery service",
+    code: "FSPIRIDONOV1C6",
+    link: "https://inst.cr/t/qEp5F7hKc",
+    categories: ["grocery"],
+  },
+  {
+    name: "Webull",
+    description: "Webull offers commission-free online stock trading",
+    code: null,
+    link: "https://act.webull.com/i/qcSfuIF1JqAI/8mw/",
+    categories: ["finance"],
+  },
+  {
+    name: "Triplebyte",
+    description: "Technical interviewing and recruiting",
+    code: null,
+    link: "https://triplebyte.com/iv/M7xiRtC/cp/header",
+    categories: ["recruiting"],
+  },
+  {
+    name: "Interviewing.io",
+    description: "Interview anonymously for real jobs",
+    code: null,
+    link: "https://iio.sh/r/GQBi",
+    categories: ["recruiting"],
+  },
+  {
+    name: "Chase Checking",
+    description: "Open a Chase Checking accont and get a bonus",
+    code: null,
+    link: "https://accounts.chase.com/raf/invite?jp_aid_p=cpo_always_on_offers%2FTile1",
+    categories: ["finance"],
+  },
+  {
+    name: "Chase United Card",
+    description: "Open a Chase United Credit and get a bonus",
+    code: null,
+    link: "https://www.referyourchasecard.com/217a/E4SGVRDAS2",
+    categories: ["finance"],
+  }
+];
