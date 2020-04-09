98d0e75dd0403a5800157b2aa4a70ada6a5151b2
Filip Spiridonov <fspiridonov@uber.com>
2020-04-08 23:06:52 -0700

    Build into docs folder

---
+++ docs/precache-manifest.5bf13b37ec40ba432c80d272a6d6fa0e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3cdc36805d536c84d4ba75f7a2f858d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cc17d5c0e27355bfffb3",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "779afa770aa16dc8e4c5",
+    "url": "/static/js/2.550fb1db.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.550fb1db.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc17d5c0e27355bfffb3",
+    "url": "/static/js/main.0e44b718.chunk.js"
+  },
+  {
+    "revision": "2d9ad17e26157cef92ac",
+    "url": "/static/js/runtime-main.e5417f57.js"
+  }
+]);
